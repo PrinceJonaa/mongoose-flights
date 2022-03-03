9349aec9817364136b55f2ca6e70ada6b1672798
princejonaa <jonathanbonner128@gmail.com>
2022-03-03 07:32:55 -0500

    update flight model & add mongoose

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,13 @@
+import { Flight } from '../models/flight.js';
+ 
 function newFlights(req, res) {
   res.render('flights/new');
 }
+function create(req,res){
+ req.body
+}
 
 export {
-  newFlights as new
+  newFlights as new,
+  create,
 }
